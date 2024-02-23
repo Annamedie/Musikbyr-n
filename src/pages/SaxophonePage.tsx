e773a71be7c7016a8a71a3d331f2ce31605d25d1
annaa <annaalehed@hotmail.com>
2024-02-23 16:05:19 +0100

    ljud och state
    Co-authored-by: David Jensen <Tsourdox@users.noreply.github.com>

--- src/pages/SaxophonePage.tsx
+++ src/pages/SaxophonePage.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 function SaxophonePage() {
+  /* state index */
+  const [audio] = useState(new Audio("src"));
+
   return (
     <>
       <h1>En saxofone</h1>
+
+      <button onClick={audio.play}>Spela ljud</button>
+
+      <div style={{ width: "10rem", overflowX: "hidden" }}>
+        <div style={{ width: "10rem" }}>
+          <img src="" alt="" style={{ width: "100%" }} />
+          <p>lorem..</p>
+        </div>
+        <img src="" alt="" style={{ width: "10rem" }} />
+        <img src="" alt="" style={{ width: "10rem" }} />
+      </div>
     </>
   );
 }
