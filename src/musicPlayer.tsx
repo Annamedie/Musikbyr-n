dc9bca96f88fe2fd4bcd869d7bf14843b283ac06
annaa <annaalehed@hotmail.com>
2024-02-24 23:41:34 +0100

    musicbox

---
+++ src/musicPlayer.tsx
@@ -0,0 +1,9 @@
+import gramphone from "./assets/gramofon.png";
+function MusicPlayer() {
+  return (
+    <div>
+      <img src={gramphone} alt="" />
+    </div>
+  );
+}
+export default MusicPlayer;
--- src/pages/SaxophonePage.tsx
+++ src/pages/SaxophonePage.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import MusicPlayer from "../MusicPlayer.tsx";
 import trumpet from "../assets/TrumpetNy.jpg";
 import clarinet from "../assets/clarinett.jpg";
 import saxophone from "../assets/saxophone3.jpg";
@@ -23,21 +25,42 @@ const infoSlider = [
 ];
 
 function SaxophonePage() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const length = infoSlider.length;
+  const next = () => {
+    setCurrentSlide(currentSlide === length - 1 ? 0 : currentSlide + 1);
+  };
+  const previous = () => {
+    setCurrentSlide(currentSlide === 0 ? length - 1 : currentSlide - 1);
+  };
   /* state index */
 
   return (
-    <div>
+    <div className="flex justify-center items-center">
+      <span onClick={previous} className="cursor-pointer">
+        ⬅️
+      </span>
       {infoSlider.map((slide, index) => {
         return (
-          <div key={slide.key}>
-            <img src={slide.image} alt="Saxofon" />
+          <div
+            key={slide.key}
+            className={`${
+              index === currentSlide ? "block" : "hidden"
+            } mx-2 flex flex-col justify-center w-72 bg-white`}
+          >
+            <img src={slide.image} alt="Instrument" className=" h-auto m-3" />
             <p>{slide.textInfo}</p>
           </div>
         );
       })}
+      <span onClick={next} className="cursor-pointer">
+        ➡️
+      </span>
+      <MusicPlayer />
     </div>
   );
 }
+
 export default SaxophonePage;
 
 /*<h1>En saxofone</h1>
