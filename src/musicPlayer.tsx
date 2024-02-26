7288c6512d9c0c4b2186dc1ca2b53b590a0414d7
annaa <annaalehed@hotmail.com>
2024-02-26 15:17:21 +0100

    tog bort

--- src/musicPlayer.tsx
+++ src/musicPlayer.tsx
@@ -23,17 +23,3 @@ function MusicPlayer({ instrumentSound }: MusicProps) {
   );
 }
 export default MusicPlayer;
-
-/*<h1>En saxofone</h1>
-
-<button onClick={audio.play}>Spela ljud</button>
-
-<div style={{ width: "10rem", overflowX: "hidden" }}>
-  <div style={{ width: "10rem" }}>
-    <img src="" alt="" style={{ width: "100%" }} />
-    <p>lorem..</p>
-  </div>
-  <img src="" alt="" style={{ width: "10rem" }} />
-  <img src="" alt="" style={{ width: "10rem" }} />
-</div>
- const [audio] = useState(new Audio("src"));*/
