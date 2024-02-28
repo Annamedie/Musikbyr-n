d7e6edf41946c4c0cfd9d46b1f5f9c9f2ce82de1
annaa <annaalehed@hotmail.com>
2024-02-28 10:13:15 +0100

    styled

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,8 +1,28 @@
+import styled from "styled-components";
+
+const Footeer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 3em;
+  background: rgb(2, 0, 36);
+  background: linear-gradient(
+    90deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(17, 9, 121, 1) 44%,
+    rgba(0, 212, 255, 1) 100%
+  );
+`;
+const H2Text = styled.h2`
+  font-size: 1.5em;
+  color: white;
+  font-family: Kalnia;
+`;
 function Footer() {
   return (
-    <div className="flex justify-center bg-emerald-400 h-10">
-      <h2>Det är footern</h2>
-    </div>
+    <Footeer>
+      <H2Text>Detta är footern</H2Text>
+    </Footeer>
   );
 }
 export default Footer;
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,8 +1,28 @@
+import styled from "styled-components";
+
+const HeadDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 3em;
+  background: rgb(2, 0, 36);
+  background: linear-gradient(
+    90deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(17, 9, 121, 1) 44%,
+    rgba(0, 212, 255, 1) 100%
+  );
+`;
+const HeadH1 = styled.h1`
+  font-size: 1.5em;
+  color: white;
+  font-family: Kalnia;
+`;
 function Header() {
   return (
-    <div className="flex justify-center bg-teal-500 h-10">
-      <h1>MUSIKBYRÅN</h1>
-    </div>
+    <HeadDiv>
+      <HeadH1>MUSIKBYRÅN</HeadH1>
+    </HeadDiv>
   );
 }
 export default Header;
