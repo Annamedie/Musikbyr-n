1886778fbb205769d141e1e5c0a7d91677e0c120
annaa <annaalehed@hotmail.com>
2024-02-29 17:48:44 +0100

    ngt

--- src/pages/InstrumentPage.tsx
+++ src/pages/InstrumentPage.tsx
@@ -1,5 +1,7 @@
-import { ChevronDoubleLeftIcon } from "@heroicons/react/16/solid";
-import { ChevronDoubleRightIcon } from "@heroicons/react/20/solid";
+import {
+  ChevronDoubleLeftIcon,
+  ChevronDoubleRightIcon,
+} from "@heroicons/react/20/solid";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
@@ -26,25 +28,36 @@ function InstrumentPage() {
       <SliderDiv>
         <ChevronDoubleLeftIcon
           onClick={previous}
-          className="cursor-pointer h-14 w-14"
-        ></ChevronDoubleLeftIcon>
-        {infoSlider.map((slide, index) => {
-          return (
-            <div
-              key={slide.id}
-              className={`${
-                index === currentSlide ? "block" : "hidden"
-              } mx-2 flex flex-col justify-center w-96 bg-cyan-500 shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]`}
-            >
-              <img src={slide.image} alt="Instrument" className=" h-auto m-3" />
-              <p>{slide.textInfo}</p>
-            </div>
-          );
-        })}
+          className="cursor-pointer h-14 w-14 absolute left-0  top-52"
+        />
+        <div className="relative flex justify-center items-center w-full">
+          {infoSlider.map((slide, index) => {
+            return (
+              <div
+                key={slide.id}
+                className={`absolute top-6 transition-opacity duration-500 ease-in ${
+                  index === currentSlide ? "opacity-100" : "opacity-0"
+                } mx-2 flex flex-col justify-center w-96 bg-cyan-500 shadow-lg`}
+                style={{
+                  transitionDelay: `${
+                    index === currentSlide ? "150ms" : "0ms"
+                  }`,
+                }}
+              >
+                <img
+                  src={slide.image}
+                  alt="Instrument"
+                  className="h-auto m-3"
+                />
+                <p>{slide.textInfo}</p>
+              </div>
+            );
+          })}
+        </div>
         <ChevronDoubleRightIcon
           onClick={next}
-          className="cursor-pointer h-14 w-14"
-        ></ChevronDoubleRightIcon>
+          className="cursor-pointer h-14 w-14 absolute right-0  top-52"
+        />
       </SliderDiv>
       <MusicPlayer instrumentSound={instrumentAudio} />
     </InstrumentDiv>
@@ -63,6 +76,8 @@ const SliderDiv = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
+  width: 35%;
 `;
 
 export default InstrumentPage;
